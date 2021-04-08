ab0034d43e06612f5983dd221e28f050183155c3
Natan <natangabriel99@yahoo.com>
2021-04-08 18:12:14 +0300

    declaration and assignment work linearly

---
+++ ScannerParser/make2.sh
@@ -0,0 +1,3 @@
+java -jar ~/Desktop/facultate/Licenta/cup/lib/java-cup-11b.jar -locations -interface -parser Parser minijava.cup
+javac -cp ~/Desktop/facultate/Licenta/cup/lib/java-cup-11b-runtime.jar:. *.java
+java -cp ~/Desktop/facultate/Licenta/cup/lib/java-cup-11b-runtime.jar:. Parser simple.minijava simple.xml /
