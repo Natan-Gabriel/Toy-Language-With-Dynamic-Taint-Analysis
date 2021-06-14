cc4226111d6fea6df3e426514dbc80e66160479a
Natan <natangabriel99@yahoo.com>
2021-06-14 19:39:22 +0300

    last commit

--- JavaToyLanguageWithTaintAnalysis/src/main/java/model/stmt/CompStmt.java
+++ JavaToyLanguageWithTaintAnalysis/src/main/java/model/stmt/CompStmt.java
@@ -9,7 +9,7 @@ public class CompStmt implements IStmt{
 	 IStmt snd;
 	 int endingLine;
 	 public CompStmt(IStmt f,IStmt s) {first=f;snd=s; endingLine = Math.max(f.getEndingLine(),s.getEndingLine());  }
-	 public String toString() { return "("+first.toString() + ";" + snd.toString()+")";}
+	 public String toString() { return "CompStmt("+first.toString() + ";" + snd.toString()+")";}
 	 public PrgState execute(PrgState state) {
 	 
 		 MyIStack<IStmt> stk=state.getStk();
--- JavaToyLanguageWithTaintAnalysis/src/main/java/view/main.java
+++ JavaToyLanguageWithTaintAnalysis/src/main/java/view/main.java
@@ -108,7 +108,7 @@ public class main {
 //
 //		IStmt ex = (IStmt) Parser.parse(arg[0]);
 //		IStmt ex = (IStmt) Parser.parse("files/goto_inside_while.txt");
-		IStmt ex = (IStmt) Parser.parse("files/dumb6.txt");
+		IStmt ex = (IStmt) Parser.parse("files/intermediate_representation.txt");
 		System.out.println(ex);
 //		ex = new CompStmt(new CompStmt(new CompStmt(new CompStmt(new VarDeclStmt("v",new RefType(new IntType()),1),
 //				new New("v",new ValueExp(new IntValue(20)),1)),
